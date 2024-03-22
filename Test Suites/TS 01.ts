00278ae58d813f064673ab0657b07db91122f65c
TriAndikaM <triandika3@gmail.com>
2024-03-22 15:16:28 +0700

    tambah ts

---
+++ Test Suites/TS 01.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS 01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b0551cef-980b-4df9-b104-041c30a89885</testSuiteGuid>
+   <testCaseLink>
+      <guid>581080db-256e-49e5-bf51-92fe91a92165</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>dce50e53-374f-479b-a43a-a09ba4cc8e46</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a3a975b3-2b0c-4f0b-ade5-dcfcdba26970</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>edb865e0-8884-44d6-9dbf-fc1aaa5519a9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
